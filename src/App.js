d4375b95e7c17e7030a6a7e1bb0ff660572cfc0b
skmayank <msaraf07@gmail.com>
2022-09-28 17:38:05 +0530

    added search functionality

--- src/App.js
+++ src/App.js
@@ -12,13 +12,25 @@ import Header from "./components/Header";
 import { moviesList } from './utility/constants';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [clickedMovie, setClickedMovie] = useState('');
+
+  useEffect(()=> {
+    if (searchValue !== '') {
+      const filtred = moviesList.filter((d) => d.Title.toLowerCase().includes(searchValue.toLowerCase()));
+      setFilteredData(filtred);
+    } else {
+      setFilteredData(moviesList);
+    }
+  }, [searchValue])
 
   return (
     <div>
       <Router>
-        <Header />
+        <Header handleChange={setSearchValue} />
         <Routes>
-          <Route path="/"  element={<Movies moviesList={moviesList} />} />
+          <Route path="/"  element={<Movies moviesList={filteredData} />} />
           <Route path="/singleMovie" element={<SingleMovie />} />
           <Route path="/gener" element={<Gener />} />
         </Routes>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,8 @@
 import React from 'react';
-// import { moviesList } from '../utility/constants';
 import Card from './Card';
 
 const Movies = ({moviesList}) => {
-  console.log("moviesList", moviesList)
+
   return (
     <div>
       {moviesList?.map(d => (
